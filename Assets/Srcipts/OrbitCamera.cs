3542c672ba03ddce8441a90e206dd357ea8cc356
martonel <65215630+martonel@users.noreply.github.com>
2026-01-18 10:33:19 +0100

    Game upload

---
+++ Assets/Srcipts/OrbitCamera.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+    public Transform orbitTarget; // Az objektum, amit figyelünk
+    public float distance = 5.0f; // A kamera távolsága az objektumtól
+    public float xSpeed = 120.0f;
+    public float ySpeed = 120.0f;
+
+    public float yMinLimit = -10f; // Minimum dõlésszög
+    public float yMaxLimit = 80f;  // Maximum dõlésszög
+
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    void Start()
+    {
+        Vector3 angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+
+        if (GetComponent<Rigidbody>() != null)
+        {
+            GetComponent<Rigidbody>().freezeRotation = true;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (orbitTarget == null) return;
+
+        if (Input.GetMouseButton(0))
+        {
+            x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+            y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+            y = ClampAngle(y, yMinLimit, yMaxLimit);
+        }
+
+        Quaternion rotation = Quaternion.Euler(y, x, 0);
+        Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
+        Vector3 position = rotation * negDistance + orbitTarget.position;
+
+        transform.rotation = rotation;
+        transform.position = position;
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F) angle += 360F;
+        if (angle > 360F) angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
